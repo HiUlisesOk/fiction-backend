35594ff516f14d21d9574553f57a8368f5a0299e
Ulises Esquivel <112531555+HiUlisesOk@users.noreply.github.com>
2023-06-19 16:47:03 -0300

    :ambulance: cambio en credenciales de auth

--- src/app.js
+++ src/app.js
@@ -57,7 +57,7 @@ server.post("/login", async (req, res, next) => {
 
 
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
